f3afb573eea73909dbebd2f834c3269169796686
Nuno Souto <nsouto@lostlink.net>
2023-03-04 13:13:35 -0500

    GithubActions , Update Octane Supervisord
    GithubActions will only run if needed, meaning files changed on each folder
    
    Octane Supervisord now has a develop mode where it watches for changed files
    
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- octane/entrypoint.sh
+++ octane/entrypoint.sh
@@ -6,6 +6,7 @@ php() {
 }
 
 initialStuff() {
+    composer dump -o; \
     php artisan optimize:clear; \
     php artisan package:discover --ansi; \
     php artisan event:cache; \
@@ -19,6 +20,7 @@ else
     initialStuff
     /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
     supercronic /etc/supercronic/laravel
+    if [ "${APP_ENV,,}" == "production" ]; then supervisorctl start octane; else supervisorctl start octane-dev; fi
     if [ "$ENABLE_HORIZON" = "true" ]; then supervisorctl start horizon; fi
     if [ "$ENABLE_SCHEDULER" = "true" ]; then supervisorctl start scheduler; fi
 fi
