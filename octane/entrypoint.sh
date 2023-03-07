c306acfa5b7f18a247fbcf44c5edf460b8e880c8
Nuno Souto <nsouto@lostlink.net>
2023-03-07 14:35:23 -0500

    Update Octane Entrypoint and fix Supervisor config copy
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- octane/entrypoint.sh
+++ octane/entrypoint.sh
@@ -17,10 +17,15 @@ initialStuff() {
 if [ "$1" != "" ]; then
     exec "$@"
 else
-    initialStuff
-    /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-    supercronic /etc/supercronic/laravel
-    if [ "${APP_ENV,,}" == "production" ]; then supervisorctl start octane; else supervisorctl start octane-dev; fi
-    if [ "$ENABLE_HORIZON" = "true" ]; then supervisorctl start horizon; fi
-    if [ "$ENABLE_SCHEDULER" = "true" ]; then supervisorctl start scheduler; fi
+  if [ -z "${APP_ENV}" ]; then
+      echo "Variable APP_ENV does not exist or is empty!"
+      echo "Setting default value to Production"
+      APP_ENV=production
+  fi
+  initialStuff
+  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+  supercronic /etc/supercronic/laravel
+  if [ "${APP_ENV,,}" == "production" ]; then supervisorctl start octane; else supervisorctl start octane-dev; fi
+  if [ "$ENABLE_HORIZON" = "true" ]; then supervisorctl start horizon; fi
+  if [ "$ENABLE_SCHEDULER" = "true" ]; then supervisorctl start scheduler; fi
 fi
