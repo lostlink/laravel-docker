97a419f5a76f79768b99463ec3763b298cc9368f
Nuno Souto <nsouto@lostlink.net>
2023-12-14 11:18:25 -0500

    Add else statement to entrypoint.sh to display message when running outside Fargate.

--- octane/usr/local/bin/entrypoint.sh
+++ octane/usr/local/bin/entrypoint.sh
@@ -9,6 +9,8 @@ initialStuff() {
   if [ "${FARGATE,,}" == "true" ]; then
     echo "Running inside Fargate adjusting DNS Resolver"
     sed -i '1s;^;nameserver 8.8.8.8\nnameserver 1.1.1.1\n;' /etc/resolv.conf
+  else
+    echo "Running outside Fargate"
   fi
   composer dump -o; \
   php artisan optimize:clear; \
