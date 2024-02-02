6ad21c523b1c24bc5192b81cd109ac7f4697cac0
Nuno Souto <nsouto@lostlink.net>
2024-02-02 09:14:47 -0500

    Update default IPV4, IPV6, MX, and NS values and modify printf statements to use these defaults.

--- pdns/var/lib/powerdns/bin/default.sh
+++ pdns/var/lib/powerdns/bin/default.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Set default variables
-default_ipv4="${PIPE_DEFAULT_IPV4}"
-default_ipv6="${PIPE_DEFAULT_IPV6}"
-default_mx="${PIPE_DEFAULT_MX}"
-default_ns="${PIPE_DEFAULT_NS:-ns1.example.com}"
+default_ipv4="${PIPE_DEFAULT_IPV4:-127.0.0.1}"
+default_ipv6="${PIPE_DEFAULT_IPV6:-::1}"
+default_mx="${PIPE_DEFAULT_MX:-localhost}"
+default_ns="${PIPE_DEFAULT_NS:-localhost}"
 
 # Read input line by line from stdin
 while IFS=$'#\t\n' read -r input; do
@@ -32,7 +32,7 @@ while IFS=$'#\t\n' read -r input; do
           printf "DATA\t%s.\tIN\tAAAA\t60\t-1\t%s\n" "$qname" "$default_ipv6"
           ;;&
         NS|ANY)
-          printf "DATA\t%s.\tIN\tNS\t60\t-1\tns1.%s\n" "$qname" "$qname"
+          printf "DATA\t%s.\tIN\tNS\t60\t-1\t%s\n" "$qname" "$default_ns"
           ;;&
         CNAME)
           printf "FAIL\n"
@@ -41,7 +41,7 @@ while IFS=$'#\t\n' read -r input; do
           printf "DATA\t%s.\tIN\tMX\t60\t-1\t10\t%s\n" "$qname" "$default_mx"
           ;;&
         SOA|ANY)
-          printf "DATA\t%s.\tIN\tSOA\t60\t-1\t%s.\thostmaster.%s\t2022042801\t10800\t3600\t604800\t3600\n" "$qname" "$default_ns" "$qname"
+          printf "DATA\t%s.\tIN\tSOA\t60\t-1\t%s.\t%s.\t1\t900\t900\t1080\t60\n" "$qname" "$default_ns" "$default_ns"
           ;;
       esac
       ;;
