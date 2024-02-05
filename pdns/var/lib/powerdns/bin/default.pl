b43749e867b4b343a8e49680d44469ef94da670b
Nuno Souto <nsouto@lostlink.net>
2024-02-05 13:12:30 -0500

    Update standard buffer settings, add error logs, modify printf statements, and enhance data outputs in default.sh, update PIPE_DEFAULT_MX in docker-compose.yml, and add coreutils and modify PowerDNS settings in Dockerfile.

---
+++ pdns/var/lib/powerdns/bin/default.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl -w
+# sample PowerDNS Coprocess backend with edns-client-subnet support
+#
+
+use strict;
+
+$|=1;					# no buffering
+
+my $line=<>;
+chomp($line);
+
+unless($line eq "HELO\t3" ) {
+	print "FAIL\n";
+	print STDERR "Received unexpected '$line', wrong ABI version?\n";
+	<>;
+	exit;
+}
+print "OK\tPerl backend firing up\n";	# print our banner
+
+while(<>)
+{
+	print STDERR "$$ Received: $_";
+	chomp();
+	my @arr=split(/\t/);
+	if(@arr < 8) {
+		print "LOG	PowerDNS sent unparseable line\n";
+		print "FAIL\n";
+		next;
+	}
+
+	# Need the root domain for the SOA record and to ignore TLD's and . queries
+
+
+	my ($type,$qname,$qclass,$qtype,$id,$ip,$localip,$ednsip)=split(/\t/);
+	my $domain = `/var/lib/powerdns/bin/powerdns domain:root $qname`;
+	my $bits=21;
+	my $auth = 1;
+
+	if (!defined($domain) || $domain eq '') {
+		print STDERR "$$ Domain is null or empty, skipping record processing\n";
+		print "END\n";
+		next;
+	}
+
+	if(($qtype eq "SOA" || $qtype eq "ANY")) {
+		print STDERR "$$ Sent SOA records\n";
+#		print "DATA	$bits	$auth	$qname	$qclass	SOA	3600	-1	ns1.domain.link hostmaster.$domain 2008080300 1800 3600 604800 3600\n";
+#		print "DATA\t$bits\t$auth\t$qname\t$qclass\tSOA\t3600\t-1\tns1.domain.link hostmaster.$domain 2008080300 1800 3600 604800 3600\n";
+
+        if (exists $ENV{"PIPE_DEFAULT_NS"}) {
+            my @ns_records = split(',', $ENV{"PIPE_DEFAULT_NS"});
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tSOA\t3600\t-1\t@ns_records[1] hostmaster.$domain 2008080300 1800 3600 604800 3600\n";
+        } else {
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tSOA\t3600\t-1\tns1.localhost hostmaster.$domain 2008080300 1800 3600 604800 3600\n";
+        }
+	}
+
+	if(($qtype eq "NS" || $qtype eq "ANY")) {
+        print STDERR "$$ Sent NS records\n";
+
+        if (exists $ENV{"PIPE_DEFAULT_NS"}) {
+            my @ns_records = split(',', $ENV{"PIPE_DEFAULT_NS"});
+            foreach my $ns (@ns_records) {
+                print "DATA\t$bits\t$auth\t$qname\t$qclass\tNS\t3600\t-1\t$ns\n";
+            }
+        } else {
+        	print "DATA\t$bits\t$auth\t$qname\t$qclass\tNS\t3600\t-1\tns1.domain.link\n";
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tNS\t3600\t-1\tns2.domain.link\n";
+        }
+	}
+
+	if(($qtype eq "TXT" || $qtype eq "ANY")) {
+		print STDERR "$$ Sent TXT records\n";
+		print "DATA\t$bits\t$auth\t$qname\t$qclass\tTXT\t3600\t-1\t\"Managed domain please visit https://domain.link!\"\n";
+	}
+
+	if(($qtype eq "A" || $qtype eq "ANY")) {
+		print STDERR "$$ Sent A records\n";
+        if (exists $ENV{"PIPE_DEFAULT_IPV4"}) {
+            my @ipv4_addresses = split(',', $ENV{"PIPE_DEFAULT_IPV4"});
+            foreach my $ipv4 (@ipv4_addresses) {
+                print "DATA\t$bits\t$auth\t$qname\t$qclass\tA\t3600\t-1\t$ipv4\n";
+            }
+        } else {
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tA\t3600\t-1\t127.0.0.1\n";
+        }
+	}
+
+	if(($qtype eq "AAAA" || $qtype eq "ANY")) {
+        print STDERR "$$ Sent AAAA records\n";
+        if (exists $ENV{"PIPE_DEFAULT_IPV6"}) {
+            my @ipv6_addresses = split(',', $ENV{"PIPE_DEFAULT_IPV6"});
+            foreach my $ipv6 (@ipv6_addresses) {
+                print "DATA\t$bits\t$auth\t$qname\t$qclass\tAAAA\t3600\t-1\t$ipv6\n";
+            }
+        } else {
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tAAAA\t3600\t-1\t::1\n";
+		}
+	}
+
+	if(($qtype eq "MX" || $qtype eq "ANY")) {
+        print STDERR "$$ Sent MX records\n";
+
+        if (exists $ENV{"PIPE_DEFAULT_MX"}) {
+            my @mx_records = split(',', $ENV{"PIPE_DEFAULT_MX"});
+            foreach my $mx_record (@mx_records) {
+                my ($priority, $mailserver) = split(' ', $mx_record);
+                print "DATA\t$bits\t$auth\t$qname\t$qclass\tMX\t3600\t-1\t$priority\t$mailserver\n";
+            }
+        } else {
+			print "DATA\t$bits\t$auth\t$qname\t$qclass\tMX\t3600\t-1\t10\tmxa.srvxx.dev\n";
+        }
+	}
+
+	print STDERR "$$ End of data\n";
+	print "END\n";
+}
--- pdns/var/lib/powerdns/bin/default.sh
+++ pdns/var/lib/powerdns/bin/default.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+#if [ yes != "$STDBUF" ]; then
+#    STDBUF=yes /usr/bin/stdbuf -i0 -o0 -e0 "$0"
+#    exit $?
+#fi
+
 # Set default variables
 default_ipv4="${PIPE_DEFAULT_IPV4:-127.0.0.1}"
 default_ipv6="${PIPE_DEFAULT_IPV6:-::1}"
 default_mx="${PIPE_DEFAULT_MX:-localhost}"
 default_ns="${PIPE_DEFAULT_NS:-localhost}"
 
+echoerr() { echo "$@" 1>&2; }
+
 # Read input line by line from stdin
 while IFS=$'#\t\n' read -r input; do
+
+  echoerr $$; echoerr $BASHPID
+  echoerr $input;
+
   # Extract command, qname, and qtype from the input in one awk call
   read -r command qname _ qtype _ <<< $(echo "$input" | awk '{print $1, $2, $3, $4, $5}')
 
@@ -21,27 +32,34 @@ while IFS=$'#\t\n' read -r input; do
     Q)
       root_domain=$(/var/lib/powerdns/bin/powerdns domain:root "$qname")
 
+      echoerr "$qname"
+
       # Handle DNS query types
       if [ "$qname" = "." ]; then
-        printf ""
+        printf "\n"
       elif [ "$root_domain" == '' ]; then
-        printf ""
+        printf "\n"
       else
         case "$qtype" in
           A|ANY)
             printf "DATA\t%s.\tIN\tA\t60\t-1\t%s\n" "$qname" "$default_ipv4"
+            echoerr "DATA\t$qname.\tIN\tA\t60\t-1\t$default_ipv4\n"
             ;;&
           AAAA|ANY)
             printf "DATA\t%s.\tIN\tAAAA\t60\t-1\t%s\n" "$qname" "$default_ipv6"
+            echoerr "DATA\t$qname.\tIN\tAAAA\t60\t-1\t$default_ipv6\n"
             ;;&
           NS|ANY)
             printf "DATA\t%s.\tIN\tNS\t60\t-1\t%s\n" "$qname" "$default_ns"
+            echoerr "DATA\t$qname.\tIN\tNS\t60\t-1\t$default_ns\n"
             ;;&
           MX|ANY)
             printf "DATA\t%s.\tIN\tMX\t60\t-1\t10\t%s\n" "$qname" "$default_mx"
+            echoerr "DATA\t$qname.\tIN\tMX\t60\t-1\t10\t$default_mx\n"
             ;;&
           SOA|ANY)
             printf "DATA\t%s.\tIN\tSOA\t60\t-1\t%s.\thostmaster.%s.\t1\t900\t900\t1080\t60\n" "$root_domain" "$default_ns" "$root_domain"
+            echoerr "DATA\t$root_domain.\tIN\tSOA\t60\t-1\t$default_ns.\thostmaster.$root_domain.\t1\t900\t900\t1080\t60\n"
             ;;
         esac
       fi
